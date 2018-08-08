21e01d4b4af8c26a9b3e796883a7a6d9a6f2e4dc
Tyler Gubala <gubalatyler@yahoo.com>
2018-08-08 18:30:06 -0400

    try adding body to the if cases

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-if [ $TRAVIS_OS_NAME == 'osx' ]; then
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     # Install some custom requirements on OS X
-    # e.g. brew install pyenv-virtualenv
+    brew install pyenv-virtualenv
 
     case "${TOXENV}" in
         py34)
             # Install some custom Python 3.4 requirements on OS X
-            
+            echo
             ;;
         py35)
             # Install some custom Python 3.5 requirements on OS X
+            echo
             ;;
         py36)
             # Install some custom Python 3.6 requirements on OS X
+            echo
             ;;
         py37)
             # Install some custom Python 3.7 requirements on OS X
+            echo
             ;;
     esac
 else
     # Install some custom requirements on Linux
+    echo
 fi
 python3 setup.py install
