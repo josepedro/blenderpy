79362f9e1609e2a5c35148836d86e053e17cd4c5
Tyler Gubala <gubalatyler@yahoo.com>
2018-08-08 18:27:21 -0400

    change the if statement minutely

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+if [ $TRAVIS_OS_NAME == 'osx' ]; then
 
     # Install some custom requirements on OS X
     # e.g. brew install pyenv-virtualenv
